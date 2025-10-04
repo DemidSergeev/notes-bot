753b79a1ec854f1a58d3ec113cba7b061668f459
Demid <myemail@foobar.com>
2025-10-04 16:37:32 +0300

    feat: import repositories to __init.py__ file

---
+++ bot/src/core/application/ports/outbound/database/__init__.py
@@ -0,0 +1,5 @@
+from .course_repository import CourseRepository
+from .subject_repository import SubjectRepository
+from .note_repository import NoteRepository
+
+__all__ = ["CourseRepository", "SubjectRepository", "NoteRepository"]
