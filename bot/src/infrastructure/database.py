f7b469f03c8941ab421ec7df6102e9a0ef345f53
Demid <myemail@foobar.com>
2025-10-05 18:48:29 +0300

    fix: correct import of models

--- bot/src/infrastructure/database.py
+++ bot/src/infrastructure/database.py
@@ -1,10 +1,11 @@
 from collections.abc import Generator
 from contextlib import contextmanager
-from typing import Protocol
 from sqlmodel import create_engine, Session, SQLModel
 
 from .config import settings
-import src.core.domain.models  # noqa: F401
+# Import of models is needed for SQLModel to generate tables from metadata
+import src.adapters.outbound.persistence.models # noqa: F401
+
 
 engine = create_engine(settings.POSTGRES_DSN, echo=True)
 
@@ -14,6 +15,7 @@ def create_db_and_tables() -> None:
 @contextmanager
 def get_session() -> Generator[Session, None, None]:
     session = Session(engine)
+
     try:
         yield session
     finally:
