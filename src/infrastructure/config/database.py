d9894558b16577289859e701a6a53279e42252ef
Demid <myemail@foobar.com>
2025-10-12 14:50:40 +0300

    feat(config): add database set up

---
+++ src/infrastructure/config/database.py
@@ -0,0 +1,22 @@
+from collections.abc import Generator
+from contextlib import contextmanager
+from sqlmodel import create_engine, Session, SQLModel
+
+from .settings import settings
+# Import of models is needed for SQLModel to generate tables from metadata
+import src.infrastructure.adapters.outbound.persistence.models # noqa: F401
+
+
+engine = create_engine(settings.POSTGRES_DSN, echo=True)
+
+def create_db_and_tables() -> None:
+    SQLModel.metadata.create_all(engine)
+
+@contextmanager
+def get_session() -> Generator[Session, None, None]:
+    session = Session(engine)
+
+    try:
+        yield session
+    finally:
+        session.close()
