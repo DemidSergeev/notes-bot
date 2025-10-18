f038a121362199f6b0263a789fb3522501db306c
Demid <myemail@foobar.com>
2025-10-18 22:41:46 +0300

    fix: add db init call to main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from src.infrastructure.config.wiring import course_commands
+from src.infrastructure.config.database import create_db_and_tables
 
 if __name__ == "__main__":
+    create_db_and_tables()
     course_commands.list_courses()
