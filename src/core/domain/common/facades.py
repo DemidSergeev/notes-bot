303942813d219f8c50c320ff2ff3e7f65b798eec
Demid <myemail@foobar.com>
2025-10-18 12:18:21 +0300

    fix: return uuid.UUID in uuid7 facade

--- src/core/domain/common/facades.py
+++ src/core/domain/common/facades.py
@@ -1,5 +1,6 @@
+import uuid
 import uuid_utils
 
 # Not really a facade. Will be removed after bumping Python to 3.14
 def uuid7():
-    return uuid_utils.uuid7()
+    return uuid.UUID(uuid_utils.uuid7().hex)
