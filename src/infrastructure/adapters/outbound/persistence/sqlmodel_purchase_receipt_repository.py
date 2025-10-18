faf2480a1462a2f2893285abc3ef503e685a788a
Demid <myemail@foobar.com>
2025-10-18 20:40:52 +0300

    feat(adapters): add purchase receipt repo

---
+++ src/infrastructure/adapters/outbound/persistence/sqlmodel_purchase_receipt_repository.py
@@ -0,0 +1,75 @@
+import uuid
+from sqlmodel import Session, select
+from collections.abc import Callable, Generator
+
+from src.core.application.ports.outbound.persistence import PurchaseReceiptRepositoryPort
+from src.core.domain.models import PurchaseReceipt, Note, Buyer
+from .models import (
+    PurchaseReceipt as DbPurchaseReceipt,
+)
+
+class SqlModelPurchaseReceiptRepository(PurchaseReceiptRepositoryPort):
+    def __init__(
+            self,
+            session_factory: Callable[[], Generator[Session, None, None]],
+        ):
+        self._session_factory = session_factory
+
+    def get_by_id(self, purchase_receipt_id: uuid.UUID) -> PurchaseReceipt | None:
+        with self._session_factory() as session:
+            session: Session
+
+            db_purchase_receipt = session.get(DbPurchaseReceipt, purchase_receipt_id)
+
+            if not db_purchase_receipt:
+                return None
+
+            return self._create_domain_purchase_receipt(db_purchase_receipt)
+
+    def get_by_buyer_id(self, buyer_id: int) -> PurchaseReceipt | None:
+        with self._session_factory() as session:
+            session: Session
+
+            statement = select(DbPurchaseReceipt).where(DbPurchaseReceipt.buyer_id == buyer_id)
+            db_purchase_receipt = session.exec(statement).first()
+
+            if not db_purchase_receipt:
+                return None
+
+            return self._create_domain_purchase_receipt(db_purchase_receipt)
+            
+    def save(self, purchase_receipt: PurchaseReceipt) -> None:
+        with self._session_factory() as session:
+            session: Session
+
+            db_purchase_receipt = DbPurchaseReceipt(
+                id=purchase_receipt.id,
+                buyer_id=purchase_receipt.buyer.external_id,
+                buyer_name=purchase_receipt.buyer.name,
+                payment_details=purchase_receipt.payment_details,
+                note_id=purchase_receipt.note.id
+            )
+
+            session.add(db_purchase_receipt)
+            session.commit()
+
+    def delete(self, purchase_receipt_id: uuid.UUID) -> None:
+        with self._session_factory() as session:
+            session: Session
+
+            db_purchase_receipt = session.get(DbPurchaseReceipt, purchase_receipt_id)
+
+            if db_purchase_receipt:
+                session.delete(db_purchase_receipt)
+                session.commit()
+
+    def _create_domain_purchase_receipt(self, db_purchase_receipt: DbPurchaseReceipt) -> PurchaseReceipt:
+        db_note = db_purchase_receipt.note
+        note = Note(id=db_note.id, title=db_note.title, price_rub=db_note.price_rub)
+        buyer = Buyer(external_id=db_purchase_receipt.buyer_id, name=db_purchase_receipt.buyer_name)
+
+        return PurchaseReceipt(
+            id=db_purchase_receipt.id,
+            note=note,
+            buyer=buyer
+        )
