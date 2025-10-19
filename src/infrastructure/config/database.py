33a9eeb068dc16a5746757ee1288f29dcbc4441d
Demid <myemail@foobar.com>
2025-10-19 13:18:40 +0300

    feat(infra): add database init

--- src/infrastructure/config/database.py
+++ src/infrastructure/config/database.py
@@ -1,7 +1,7 @@
 from collections.abc import Generator
 from contextlib import contextmanager
 from time import sleep
-from sqlmodel import create_engine, Session, SQLModel
+from sqlmodel import create_engine, Session, SQLModel, select
 
 from .settings import settings
 # Import of models is needed for SQLModel to generate tables from metadata
@@ -22,3 +22,31 @@ def get_session() -> Generator[Session, None, None]:
         yield session
     finally:
         session.close()
+
+def init_database() -> None:
+    create_db_and_tables()
+    if not has_initial_data():
+        from .wiring import course_service
+        from src.core.domain.models import Subject
+        from src.core.domain.common.enums import CourseYear
+
+        subjects_year_one = [
+            Subject(name="Math"),
+            Subject(name="Science"),
+            Subject(name="History"),
+        ]
+        course_service.create(subjects=subjects_year_one, year=CourseYear.ONE)
+        subjects_year_two = [
+            Subject(name="Algebra"),
+            Subject(name="Biology"),
+            Subject(name="World History"),
+        ]
+        course_service.create(subjects=subjects_year_two, year=CourseYear.TWO)
+
+def has_initial_data() -> bool:
+    with get_session() as session:
+        session: Session
+
+        statement = select(SQLModel.metadata.tables['course'])
+        result = session.exec(statement).first()
+        return result is not None
