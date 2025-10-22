7b697a4fd797242ce1c659d51f5a2c68a18ed649
Demid <myemail@foobar.com>
2025-10-22 15:42:02 +0300

    feat: run application in main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
-from src.infrastructure.config.wiring import course_commands
+from src.infrastructure.config.wiring import application
 from src.infrastructure.config.database import init_database
+from src.infrastructure.config.logger import setup_logging
 
 if __name__ == "__main__":
+    setup_logging()
     init_database()
-    course_commands.list_courses()
+    application.setup()
+    application.run()
