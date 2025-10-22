8f23e127f9e750445a42573cd9cd4046f2daaab3
Demid <myemail@foobar.com>
2025-10-22 15:39:09 +0300

    feat(ports): update purchase service port

--- src/core/application/ports/inbound/purchase_service_port.py
+++ src/core/application/ports/inbound/purchase_service_port.py
@@ -1,10 +1,18 @@
+import uuid
 from typing import Protocol
 
-from src.core.domain.models import Course, Note, Buyer, PurchaseReceipt
+from src.core.domain.models import Course, Subject, Note, Buyer, PurchaseReceipt
+from src.core.domain.common.enums import CourseYear
 
 class PurchaseServicePort(Protocol):
     def get_courses(self) -> list[Course]:
         raise NotImplementedError
 
-    def generate_purchase_receipt(self, note: Note, buyer: Buyer) -> PurchaseReceipt:
+    def get_subjects(self, course_year: CourseYear) -> list[Subject]:
+        raise NotImplementedError
+
+    def get_notes(self, subject_id: uuid.UUID) -> list[Note]:
+        raise NotImplementedError
+
+    def generate_purchase_receipt(self, note_id: uuid.UUID, buyer: Buyer) -> PurchaseReceipt:
         raise NotImplementedError
