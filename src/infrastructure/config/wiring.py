073f2bdbff276d2578184a10c4b369b0ec769ee3
Demid <myemail@foobar.com>
2025-10-22 15:41:49 +0300

    feat(infra): add tg components to wiring

--- src/infrastructure/config/wiring.py
+++ src/infrastructure/config/wiring.py
@@ -1,9 +1,10 @@
-from src.infrastructure.adapters.inbound.commands import CliCourseCommands
 from src.core.application.services import PurchaseService
-from src.core.application.services.data import CourseService, PurchaseReceiptService
 from src.infrastructure.adapters.outbound.persistence import SqlModelCourseRepository, SqlModelSubjectRepository, SqlModelNoteRepository, SqlModelPurchaseReceiptRepository
 from src.infrastructure.adapters.outbound.payment_details_provider import ConfigPaymentDetailsProvider
+from src.infrastructure.adapters.inbound.telegram import TelegramHandlers
+from src.infrastructure.adapters.inbound.telegram import Application
 from src.infrastructure.config.database import get_session
+from src.infrastructure.config import settings
 
 
 note_repo = SqlModelNoteRepository(session_factory=get_session)
@@ -11,13 +12,20 @@ subject_repo = SqlModelSubjectRepository(session_factory=get_session, note_repos
 course_repo = SqlModelCourseRepository(session_factory=get_session, subject_repository=subject_repo)
 purchase_receipt_repo = SqlModelPurchaseReceiptRepository(session_factory=get_session)
 
-course_service = CourseService(course_repo=course_repo, subject_repo=subject_repo)
-purchase_receipt_service = PurchaseReceiptService(purchase_receipt_repo=purchase_receipt_repo)
-
 purchase_service = PurchaseService(
-    course_service=course_service,
-    purchase_receipt_service=purchase_receipt_service,
+    course_repo=course_repo,
+    subject_repo=subject_repo,
+    note_repo=note_repo,
+    purchase_receipt_repo=purchase_receipt_repo,
     payment_details_provider=ConfigPaymentDetailsProvider(),
 )
 
-course_commands = CliCourseCommands(purchase_service)
+telegram_handlers = TelegramHandlers(
+    purchase_service=purchase_service,
+    welcome_message="Добро пожаловать в бот по покупке конспектов!"
+)
+
+application = Application(
+    telegram_handlers=telegram_handlers,
+    telegram_token=settings.TELEGRAM_BOT_TOKEN
+)
