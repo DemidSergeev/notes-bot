2497ee18f722a56ba9f31e1c24c8787b657de7b3
Demid <myemail@foobar.com>
2025-11-08 10:04:15 +0300

    feat(ports): add get url to note storage port

--- src/core/application/ports/outbound/storage/note_storage_port.py
+++ src/core/application/ports/outbound/storage/note_storage_port.py
@@ -7,7 +7,10 @@ from src.core.domain.models import Note
 class NoteStoragePort(Protocol):
     def get_by_id(note_id: uuid.UUID) -> bytes | None:
         raise NotImplementedError
-    
+
+    def get_url(note_id: uuid.UUID) -> str | None:
+        raise NotImplementedError
+
     def save(note: Note, file: bytes) -> None:
         raise NotImplementedError
     
