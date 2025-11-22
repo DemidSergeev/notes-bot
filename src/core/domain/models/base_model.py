845f829c7ed00a6c3393d82839db07cf3d2bd8e6
Demid.Sergeev <bviiidontpanic@gmail.com>
2025-11-22 14:57:51 +0300

    style: add newlines at end

--- src/core/domain/models/base_model.py
+++ src/core/domain/models/base_model.py
@@ -10,3 +10,4 @@ class BaseModel:
     # This avoids errors with unexpected keyword argument 'id' and non-default argument following default argument 'id'
     _: KW_ONLY
     id: uuid.UUID = field(default_factory=uuid7)
+ 
