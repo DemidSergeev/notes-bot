013b186856bc35ea6b9e98af3d21ec39263ba4a3
Demid.Sergeev <bviiidontpanic@gmail.com>
2025-11-29 15:13:40 +0300

    refactor: remove adding mock subjects to db on init

--- src/infrastructure/config/database.py
+++ src/infrastructure/config/database.py
@@ -28,45 +28,13 @@ def get_session() -> Generator[Session, None, None]:
 
 def init_database() -> None:
     create_db_and_tables()
-    if not has_initial_data():
-        from .wiring import course_repo
-        from src.core.domain.models import Course, Subject, Note
-        from src.core.domain.common.enums import CourseYear
+    from .wiring import course_repo
+    from src.core.domain.models import Course
+    from src.core.domain.common.enums import CourseYear
 
-        subjects_year_one = [
-            Subject(name="Math", notes=[
-                Note(title="Limits and derivatives", price_rub=99),
-                Note(title="Linear equations", price_rub=79)
-            ]),
-            Subject(name="Science", notes=[
-                Note(title="Physics Notes", price_rub=89)
-            ]),
-            Subject(name="History", notes=[
-                Note(title="World War II Notes", price_rub=79)
-            ]),
-        ]
-        course_year_one = Course(
-            year=CourseYear.ONE,
-            subjects=subjects_year_one
-        )
-        course_repo.save(course=course_year_one)
-
-        subjects_year_two = [
-            Subject(name="Algebra"),
-            Subject(name="Biology"),
-            Subject(name="World History"),
-        ]
-        course_year_two = Course(
-            year=CourseYear.TWO,
-            subjects=subjects_year_two
-        )
-        course_repo.save(course=course_year_two)
-        logging.info("Initial data in DB created")
-
-def has_initial_data() -> bool:
-    with get_session() as session:
-        session: Session
-
-        statement = select(SQLModel.metadata.tables['course'])
-        result = session.exec(statement).first()
-        return result is not None
+    for year in CourseYear:
+        existing_course = course_repo.get_by_year(year)
+        if existing_course is None:
+            new_course = Course(year=year)
+            course_repo.save(course=new_course)
+            logging.debug("Course %d created", year.value)
