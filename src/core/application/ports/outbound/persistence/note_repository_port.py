ac3c35ce281665211377bf06182de67cae989770
Demid.Sergeev <bviiidontpanic@gmail.com>
2025-12-13 21:22:29 +0300

    feat: use ellipsis (...) instead of raising NotImplementedError
    Mainly for 100% test coverage of those modules

--- src/core/application/ports/outbound/persistence/note_repository_port.py
+++ src/core/application/ports/outbound/persistence/note_repository_port.py
@@ -6,19 +6,19 @@ from src.core.domain.models import Note
 
 class NoteRepositoryPort(Protocol):
     def get_by_id(self, note_id: uuid.UUID) -> Note | None:
-        raise NotImplementedError
+        ...
 
     def get_by_title(self, title: str) -> Note | None:
-        raise NotImplementedError
+        ...
 
     def get_approved_by_subject_id(self, subject_id: uuid.UUID) -> list[Note]:
-        raise NotImplementedError
+        ...
 
     def get_not_approved(self) -> list[Note]:
-        raise NotImplementedError
+        ...
 
     def save(self, note: Note, subject_id: uuid.UUID) -> None:
-        raise NotImplementedError
+        ...
 
     def delete(self, note_id: uuid.UUID) -> None:
-        raise NotImplementedError
+        ...
